dc54846d8c79f87ed7d4a7d93d15b98e809a511e
Augustin Briolon <augustin.briolon@fortuneo.com>
2023-11-20 16:53:58 +0100

    first commit

---
+++ AppleDevices/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  AppleDevices
+//
+//  Created by AUGUSTIN BRIOLON on 20/11/2023.
+//
+
+import Foundation
---
+++ AppleDevices/NewDeviceScreen.swift
@@ -0,0 +1,8 @@
+//
+//  NewDeviceScreen.swift
+//  AppleDevices
+//
+//  Created by AUGUSTIN BRIOLON on 20/11/2023.
+//
+
+import Foundation
