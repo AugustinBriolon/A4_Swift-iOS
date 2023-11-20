f226199e856740556a045e733c35bf1dbcedd011
Augustin Briolon <augustin.briolon@fortuneo.com>
2023-11-20 13:52:39 +0100

    Initial Commit

---
+++ AppleDevices/AppleDevicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleDevicesApp.swift
+//  AppleDevices
+//
+//  Created by AUGUSTIN BRIOLON on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleDevicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
